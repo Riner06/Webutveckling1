b82be01357be1ffb2da38101ad7c91b84f58847f
Riner06 <melker@safari.se>
2024-02-15 11:06:12 +0100

    Responsivitet övning

--- Responsivitet och media queries/script.js
+++ Responsivitet och media queries/script.js
@@ -1,16 +1,24 @@
 
 window.onload = function() {
     let menu = document.getElementById("menu");
-    menu.style.display = "none";
+     // menu.style.display = "block";
+    if (window.matchMedia("(max-width: 993px)").matches) {
+        // If the screen size is 599px or less, hide the menu
+        menu.style.display = "none";
+    } else {
+        // Otherwise, show the menu
+        menu.style.display = "block";
+    }
 }
 
 
-function expandProjectsList(element) {
+function expandProjectsList() {
     let menu = document.getElementById("menu");
-    if (menu.style.display === "none") {
+    if (menu.style.display === "none" ) {
         
         menu.style.display = "block";
     } else {
         menu.style.display = "none";
     }
+    
 }
